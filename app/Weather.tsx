e126eaa9b3530bd4fe7a5ca96e68e585f6bf2798
victorias <victoria.s.sun@gmail.com>
2023-06-26 15:13:18 -0700

    Add basic weather chart with precipitation, temperature, and windspeed.

--- app/Weather.tsx
+++ app/Weather.tsx
@@ -1,14 +1,62 @@
 "use client";
-import { VisualCrossingResponse } from "@/api/types";
+import { Hour, VisualCrossingResponse } from "@/api/types";
 import { QueryFunctionContext, useQuery } from "@tanstack/react-query";
 import { startOfWeek, add, format } from "date-fns";
+import { useMemo } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "@/tailwind.config";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const chartOptions = {
+  response: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: false,
+    },
+  },
+  scales: {
+    y: {
+      display: false,
+    },
+    x: {
+      grid: {
+        // display: false,
+      },
+    },
+  },
+};
+
+const fullConfig = resolveConfig(tailwindConfig);
 
 const fetchWeatherData = async (
   location: string,
   onDate: string
 ): Promise<VisualCrossingResponse> => {
   const res = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${onDate}/?unitGroup=us&key=WAAK2EVX4LK7SB5FSJXLSWTVK&contentType=json`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${onDate}/?unitGroup=us&key=WAAK2EVX4LK7SB5FSJXLSWTVK&contentType=json&include=hours`,
     {
       method: "GET",
     }
@@ -19,6 +67,47 @@ const fetchWeatherData = async (
   }
 
   return res.json();
+};
+
+const WeatherChart = ({
+  responseData,
+}: {
+  responseData: VisualCrossingResponse;
+}) => {
+  const labels = responseData.days[0].hours.map((hour) => hour.datetime);
+
+  return (
+    <Line
+      options={chartOptions}
+      data={{
+        labels,
+        datasets: [
+          {
+            label: "Temperature (°F)",
+            data: responseData.days[0].hours.map((hour) => hour.temp),
+            cubicInterpolationMode: "default",
+            borderColor: fullConfig.theme?.colors?.red["200"],
+            backgroundColor: fullConfig.theme?.colors?.red["200"],
+          },
+          {
+            label: "Windspeed (mph)",
+            data: responseData.days[0].hours.map((hour) => hour.windspeed),
+            cubicInterpolationMode: "default",
+            borderColor: fullConfig.theme?.colors.green["200"],
+            backgroundColor: fullConfig.theme?.colors.green["200"],
+          },
+
+          {
+            label: "Precipitation",
+            cubicInterpolationMode: "default",
+            data: responseData.days[0].hours.map((hour) => hour.precip),
+            borderColor: fullConfig.theme?.colors.blue["200"],
+            backgroundColor: fullConfig.theme?.colors.blue["200"],
+          },
+        ],
+      }}
+    />
+  );
 };
 
 const Weather = () => {
@@ -65,7 +154,12 @@ const Weather = () => {
   }
   console.log(data);
 
-  return <div>{data.address}</div>;
+  return (
+    <div className="h-screen">
+      {data.address}
+      <WeatherChart responseData={data} />
+    </div>
+  );
 };
 
 export default Weather;
