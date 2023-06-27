424fc3b9f6c209da92c08d0dba6dbc48dd5dd186
victorias <victoria.s.sun@gmail.com>
2023-06-27 11:19:35 -0700

    Fix Z index on Dropdown

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -72,7 +72,7 @@ const Dropdown = ({
       </div>
 
       {isOpen && (
-        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+        <div className="z-50 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
           <div
             className="py-1"
             role="menu"
